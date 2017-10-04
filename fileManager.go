522173cee7b72eed9936816dcab98621d6712271
Jayson Grace <jayson.e.grace@gmail.com>
2017-10-03 20:18:57 -0700

    Add fileManager for file I/O operations

---
+++ fileManager.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// readLines reads an input file into memory from the specified path.
+//
+// Upon successfully reading the file in, it will return the lines which
+// make up the file in the form of a []string.
+func readLines(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+// writeLines writes an input slice to the specified file.
+func writeLines(lines []string, filePath string) error {
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+	return w.Flush()
+}
